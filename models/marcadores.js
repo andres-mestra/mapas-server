a107c80a903a6a009aa3b3ff3cebe8c9bc116091
andresdev <andresmestra11a@gmail.com>
2021-02-09 19:02:12 -0500

    Modelos de marcadores y integración con socket.io

---
+++ models/marcadores.js
@@ -0,0 +1,28 @@
+
+
+class Marcadores {
+
+  constructor(){
+    /*
+    activos = {
+      [Marcador.id]:{Marcador}
+    }*/
+    this.activos = {};
+  }
+
+  agregarMarcador( marcador ) {
+    this.activos[marcador.id] = marcador;
+    return marcador;
+  }
+
+  removerMarcador( id ) {
+    delete this.activos[id];
+  }
+
+  actualizarMarcador( marcador ) {
+    this.activos[marcador.id] = marcador;
+  }
+
+}
+
+module.exports =  Marcadores;
